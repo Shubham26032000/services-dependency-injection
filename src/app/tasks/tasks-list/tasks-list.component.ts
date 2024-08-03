4d60eee9fe5371a08acab45080fa45eb81e23664
Shubham Vast <shubhamvast5@gmail.com>
2024-08-03 21:11:43 +0530

    Using DI & Services select task based on filter added

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TaskService } from '../task.service';
@@ -14,7 +14,24 @@ export class TasksListComponent {
   private taskService = inject(TaskService);
   selectedFilter = signal<string>('all');
 
-  tasks = this.taskService.allTasks;
+  tasks = computed(() => {
+    switch (this.selectedFilter()) {
+      case 'open':
+        return this.taskService
+          .allTasks()
+          .filter((task) => task.status === 'OPEN');
+      case 'in-progress':
+        return this.taskService
+          .allTasks()
+          .filter((task) => task.status === 'IN_PROGRESS');
+      case 'done':
+        return this.taskService
+          .allTasks()
+          .filter((task) => task.status === 'DONE');
+      default:
+        return this.taskService.allTasks();
+    }
+  });
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
